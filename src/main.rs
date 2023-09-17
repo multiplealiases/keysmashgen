bf47f2dde9b1617e29eba49834a6c34296b04d65
multiplealiases <conflictvegetable@protonmail.com>
2023-09-17 18:22:13 +0800

    first working, 1-hand version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use rand::{thread_rng, Rng};
+
 fn main() {
-    println!("Hello, world!");
+    keysmash();
+}
+
+fn keysmash() {
+    let mut keysmash = [1, 2, 3, 4].to_vec();
+    let mut cycles = 30;
+    let mut rng = thread_rng();
+    while cycles != 0 {
+        let a: usize = rng.gen_range(0..4);
+        let b: usize = rng.gen_range(0..4);
+        let s = keysmash.iter().map(|n| n.to_string()).collect::<String>();
+        print!("{s}");
+        cycles = cycles - 1;
+        keysmash.swap(a, b);
+        }
+    println!("");
 }
