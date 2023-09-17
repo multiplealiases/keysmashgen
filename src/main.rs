9008d65e34fe05f7ff0f78ae957f6e0284e7499f
multiplealiases <conflictvegetable@protonmail.com>
2023-09-17 21:04:42 +0800

    threaded keysmash

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    let mut hand1 = ["1", "2", "3", "4"].to_vec();
-    let mut counter1 = 0;
-    for _ in 1..20 {
-        (hand1, counter1) = keysmash(hand1.to_vec(), counter1);
+    let hands = hands.as_slice();
+    let mut children = vec![];
+    for t in 0..hands.len() {
+        println!("spawning thread {t}");
+        children.push(thread::spawn(move || {
+            let mut hand: Vec<&str> = hands[t].to_vec();
+            let mut counter = 0;
+            for _ in 0..50 {
+                (hand, counter) = keysmash(hand, counter);
+                thread::sleep(Duration::from_millis(1));
+            }
+        }));
+    }
+
+    for child in children {
+        child.join().unwrap();
     }
 }
 
@@ -10,7 +25,7 @@ fn keysmash(mut keysmash: Vec<&str>, mut counter: usize) -> (Vec<&str>, usize) {
     let s = keysmash[counter];
     print!("{s}");
     counter = counter + 1;
-    if counter == keysmash.len() - 1 {
+    if counter == keysmash.len() {
         counter = 0;
         keysmash.swap(1, 3);
     }
