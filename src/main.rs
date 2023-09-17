66d95c9b3ffe2eba82f6fcf85c881780cc9f0113
multiplealiases <conflictvegetable@protonmail.com>
2023-09-17 18:43:04 +0800

    possibly thread-safe

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,18 @@
-use rand::{thread_rng, Rng};
-
 fn main() {
-    keysmash();
+    let mut hand1 = ["1", "2", "3", "4"].to_vec();
+    let mut counter1 = 0;
+    for _ in 1..20 {
+        (hand1, counter1) = keysmash(hand1.to_vec(), counter1);
+    }
 }
 
-fn keysmash() {
-    let mut keysmash = [1, 2, 3, 4].to_vec();
-    let mut cycles = 30;
-    let mut rng = thread_rng();
-    while cycles != 0 {
-        let a: usize = rng.gen_range(0..4);
-        let b: usize = rng.gen_range(0..4);
-        let s = keysmash.iter().map(|n| n.to_string()).collect::<String>();
-        print!("{s}");
-        cycles = cycles - 1;
-        keysmash.swap(a, b);
-        }
-    println!("");
+fn keysmash(mut keysmash: Vec<&str>, mut counter: usize) -> (Vec<&str>, usize) {
+    let s = keysmash[counter];
+    print!("{s}");
+    counter = counter + 1;
+    if counter == keysmash.len() - 1 {
+        counter = 0;
+        keysmash.swap(1, 3);
+    }
+    (keysmash, counter)
 }
