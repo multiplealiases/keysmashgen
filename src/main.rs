685e84d1ad7844647574d25c8866a090daea4824
multiplealiases <conflictvegetable@protonmail.com>
2023-09-17 21:45:48 +0800

    vector hands??

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    let hands = hands.as_slice();
-    let mut children = vec![];
+    let hands = vec![vec!["a", "b", "c", "d"], vec!["e", "f", "g", "h"]];
+    let mut children = Vec::new();
+
     for t in 0..hands.len() {
         println!("spawning thread {t}");
+        let hands = hands.clone();
         children.push(thread::spawn(move || {
             let mut hand: Vec<&str> = hands[t].to_vec();
             let mut counter = 0;
@@ -27,7 +29,6 @@ fn keysmash(mut keysmash: Vec<&str>, mut counter: usize) -> (Vec<&str>, usize) {
     counter = counter + 1;
     if counter == keysmash.len() {
         counter = 0;
-        keysmash.swap(1, 3);
     }
     (keysmash, counter)
 }
