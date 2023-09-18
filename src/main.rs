1a0f29fafd9e09e15eec54e6c99ce653c4d2553c
multiplealiases <conflictvegetable@protonmail.com>
2023-09-18 12:38:36 +0800

    convert to char

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,14 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    let hands = vec![vec!["a", "b", "c", "d"], vec!["e", "f", "g", "h"]];
+    let hands = vec![vec!['a', 'b', 'c', 'd'], vec!['e', 'f', 'f', 'f']];
     let mut children = Vec::new();
 
     for t in 0..hands.len() {
         println!("spawning thread {t}");
         let hands = hands.clone();
         children.push(thread::spawn(move || {
-            let mut hand: Vec<&str> = hands[t].to_vec().clone();
+            let mut hand: Vec<char> = hands[t].to_vec().clone();
             let mut counter = 0;
             for _ in 0..50 {
                 (hand, counter) = keysmash(hand, counter);
@@ -23,7 +23,7 @@ fn main() {
     }
 }
 
-fn keysmash(mut keysmash: Vec<&str>, mut counter: usize) -> (Vec<&str>, usize) {
+fn keysmash(mut keysmash: Vec<char>, mut counter: usize) -> (Vec<char>, usize) {
     let s = keysmash[counter];
     print!("{s}");
     counter = counter + 1;
