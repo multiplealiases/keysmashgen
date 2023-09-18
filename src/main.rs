60f871c6193ef088f0ffb0d6307dff555928aa6e
multiplealiases <conflictvegetable@protonmail.com>
2023-09-18 15:45:01 +0800

    cromulent

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,52 @@
+use clap::Parser;
 use rand::{thread_rng, Rng};
+use std::process::ExitCode;
 use std::thread;
 use std::time::Duration;
+/// Generates cromulent keysmash
+#[derive(Parser, Debug)]
+#[clap(version, about)]
+#[command(name = "keysmashgen")]
+#[command(version = "0.1.0")]
+struct Args {
+    /// number of keypresses per hand
+    #[clap(short, long, default_value_t = 50)]
+    length: usize,
+    /// probability of mutation every cycle of keysmash
+    #[clap(short, long, default_value_t = 1.0)]
+    probability: f32,
+    /// keys that each finger of a hand is on. May be specified multiple times for multiple hands
+    hands: Vec<String>,
+}
 
-fn main() {
+fn main() -> ExitCode {
+    let args = Args::parse();
+    let length: usize = args.length;
+    let probability: f32 = args.probability;
+
+    match probability {
+        p if !(0.0..=1.0).contains(&p) => {
+            println!("probability must be between 0 to 1 (inclusive)");
+            return ExitCode::from(1);
+        }
+        _ => (),
+    }
+
     let mut children = Vec::new();
-    for hand in std::env::args().skip(1) {
+    for hand in args.hands {
         children.push(thread::spawn(move || {
-            let mut counter = 0;
-            let mut rng = thread_rng();
             let handlen = hand.len();
-            for _ in 0..50 {
+            let mut counter = handlen;
+            let mut rng = thread_rng();
+            for _ in 0..length {
                 let mut hand: Vec<char> = hand.chars().collect::<Vec<char>>();
-                counter = keysmash(hand.clone(), counter);
                 if counter == handlen {
                     counter = 0;
-                    hand.swap(rng.gen_range(0..handlen), rng.gen_range(0..handlen));
+                    if rng.gen::<f32>() < probability {
+                        hand.swap(rng.gen_range(0..handlen), rng.gen_range(0..handlen));
+                    }
                 }
+                counter = keysmash(hand.clone(), counter);
                 thread::sleep(Duration::from_millis(1));
             }
         }));
@@ -23,6 +54,7 @@ fn main() {
     for child in children {
         child.join().unwrap();
     }
+    ExitCode::from(0)
 }
 
 fn keysmash(keysmash: Vec<char>, counter: usize) -> usize {
